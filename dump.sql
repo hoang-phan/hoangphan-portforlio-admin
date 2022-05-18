4eed3a24cc1842904965da99908c5c94d1f7254c
Hoang Phan <hoang@netengine.com.au>
2022-05-18 20:55:10 +0700

    Update

--- dump.sql
+++ dump.sql
@@ -278,6 +278,82 @@ CREATE TABLE public.schema_migrations (
 ALTER TABLE public.schema_migrations OWNER TO hoang;
 
 --
+-- Name: taggings; Type: TABLE; Schema: public; Owner: hoang
+--
+
+CREATE TABLE public.taggings (
+    id bigint NOT NULL,
+    tag_id integer,
+    taggable_type character varying,
+    taggable_id integer,
+    tagger_type character varying,
+    tagger_id integer,
+    context character varying(128),
+    created_at timestamp without time zone,
+    tenant character varying(128)
+);
+
+
+ALTER TABLE public.taggings OWNER TO hoang;
+
+--
+-- Name: taggings_id_seq; Type: SEQUENCE; Schema: public; Owner: hoang
+--
+
+CREATE SEQUENCE public.taggings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.taggings_id_seq OWNER TO hoang;
+
+--
+-- Name: taggings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hoang
+--
+
+ALTER SEQUENCE public.taggings_id_seq OWNED BY public.taggings.id;
+
+
+--
+-- Name: tags; Type: TABLE; Schema: public; Owner: hoang
+--
+
+CREATE TABLE public.tags (
+    id bigint NOT NULL,
+    name character varying,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL,
+    taggings_count integer DEFAULT 0
+);
+
+
+ALTER TABLE public.tags OWNER TO hoang;
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE; Schema: public; Owner: hoang
+--
+
+CREATE SEQUENCE public.tags_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tags_id_seq OWNER TO hoang;
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hoang
+--
+
+ALTER SEQUENCE public.tags_id_seq OWNED BY public.tags.id;
+
+
+--
 -- Name: active_storage_attachments id; Type: DEFAULT; Schema: public; Owner: hoang
 --
 
@@ -320,12 +396,29 @@ ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.pro
 
 
 --
+-- Name: taggings id; Type: DEFAULT; Schema: public; Owner: hoang
+--
+
+ALTER TABLE ONLY public.taggings ALTER COLUMN id SET DEFAULT nextval('public.taggings_id_seq'::regclass);
+
+
+--
+-- Name: tags id; Type: DEFAULT; Schema: public; Owner: hoang
+--
+
+ALTER TABLE ONLY public.tags ALTER COLUMN id SET DEFAULT nextval('public.tags_id_seq'::regclass);
+
+
+--
 -- Data for Name: active_storage_attachments; Type: TABLE DATA; Schema: public; Owner: hoang
 --
 
 COPY public.active_storage_attachments (id, name, record_type, record_id, blob_id, created_at) FROM stdin;
 2	logo	Company	1	2	2022-04-06 13:05:16.60696
 3	logo	Company	2	3	2022-04-06 13:39:05.274409
+4	image	Project	6	4	2022-05-18 12:34:55.841064
+5	image	Project	7	5	2022-05-18 13:06:09.818341
+6	image	Project	8	6	2022-05-18 13:15:44.481183
 \.
 
 
@@ -336,6 +429,9 @@ COPY public.active_storage_attachments (id, name, record_type, record_id, blob_i
 COPY public.active_storage_blobs (id, key, filename, content_type, metadata, service_name, byte_size, checksum, created_at) FROM stdin;
 2	82nsse04c2smwqrn13z62i8a06k6	east-agile-logo_70x70.png	image/png	{"identified":true,"analyzed":true}	cloudinary	1042	MPUFVALbldTzmtMyfEfUiw==	2022-04-06 13:05:16.601743
 3	leg0qug68uqf9nmkrcx3v81kdtqv	ne_70x70.png	image/png	{"identified":true,"analyzed":true}	cloudinary	1647	AsSDCBYsn29Sp8ToReEBAQ==	2022-04-06 13:39:05.269
+4	tny1sh2x5hv1x4qbrdcd87ibkgb9	e1g12l8y50mo0pgfa1zi6ti03pg0.png	image/png	{"identified":true,"analyzed":true}	cloudinary	21870	B6cQcQVLryGyERvVBAhlaw==	2022-05-18 12:34:55.769301
+5	25ophgmbsg7pw2lzl0nz6dgppnyv	i67v522nbwurt8e6cqqvmjfjfy83.png	image/png	{"identified":true,"analyzed":true}	cloudinary	116098	g1tmU5yhg3SDidkEGCFZQA==	2022-05-18 13:06:09.813626
+6	89t5qw1i9fv6iu9n5bwn39p8x8rz	yeuftvcewnou3gxe63prjw1wac9c.png	image/png	{"identified":true,"analyzed":true}	cloudinary	490751	WabHFY0yvaPJax9+FRJKTg==	2022-05-18 13:15:44.47706
 \.
 
 
@@ -380,11 +476,14 @@ COPY public.companies (id, name, from_year, to_year, location, created_at, updat
 --
 
 COPY public.projects (id, company_id, name, from_year, to_year, summary, challenge, resolution, created_at, updated_at, code_url, live_url) FROM stdin;
+6	\N	Flappy Bird AI	2020	2021	Constructing an AI for Flappy Bird to play the game itself.	We need the bird to be able to make decision which action they should take at a given time through unsupervised learning and evolution.	We implement neural networks as brains for the birds. For each generation, we mutate the candidates, let them compete and do the natural selection.	2022-05-18 12:34:55.607158	2022-05-18 12:36:45.986176	https://github.com/hoang-phan/flappy_bird_ai	https://flappybirdai.onrender.com/
 4	2	Padmore City	2018	2022	Learning website where providers offer online courses in form of interesting games	Ability to support questions/answers as casual conversation, rating task and award achievement automatically	Implement varies of conversations and multi-choice questions, configurable achievement system based on completed tasks metrics	2022-04-11 12:46:56.455723	2022-04-11 12:56:19.337695	\N	\N
-3	2	Scout Onboard	2018	2022	Complete solution for HR users to manage the onboarding process of eligible candidates	To be able to create and customize the offer letter, approval process, ability for candidate to sign and accept as well as negotiate the offer	Take advantage of Rails event machine and Mustache placeholders for offer process, JS signature pad and canvas manipulation to capture candidate signature. Rendering and saving base64 image of the signatures to db.	2022-04-11 12:35:59.454419	2022-04-11 12:59:32.48764	\N	\N
 2	2	Scout Recruit	2016	2022	Complete solution for HR users to post job ad, recruit candidates and integrate with other platforms	Biggest project so far in my career with a wide variety of features: Questionnaire, Booking system, Email/SMS automation, 3rd party integrations...	Make a wide variety of questions in job ads with auto-rating metrics. Use Twilio/Sendgrind for SMS/Emails. Implement each integration according to their public/private API documentation	2022-04-11 12:23:40.94895	2022-04-11 13:07:03.600068	\N	\N
 1	1	OppSites	2014	2016	Real estate website for the landlords to advertise their properties visually in the map.	Ability to display the lands' borders on the map, query by longitude, latitude and nearby properties.	Use Google Map JS API to draw polygons, save them along with properties' information to PostgreSQL and use PostGIS to query	2022-04-11 12:10:40.222886	2022-04-11 13:08:29.804923	\N	\N
 5	2	CompliLearn	2018	2019	Learning website for schools to manage their courses and staff	Build 3rd party login for Go1 and different school portals	Use Auth0 with Custom Social Login scripts to configure OAuth for Go1/school portals	2022-04-11 12:55:30.244273	2022-04-11 13:09:32.89567	\N	\N
+7	\N	Sudoku Solver	2021	2022	A web app letting user input their sudoku by numbers or image and solve it	The biggest challenge is how to read the sudoku from image, I also want to improve the sudoku algorithm on the internet so it is faster for every input combination.	I derive an improved Sudoku algorithm that gives dramatic performance gain over the solution on the Internet, use Python OpenCV and Tesseract OCR to read input from image.	2022-05-18 13:06:08.683333	2022-05-18 13:06:09.82224	https://github.com/hoang-phan/sudoku-solver	http://hoang-sudoku-solver.herokuapp.com/
+3	2	Scout Onboard	2018	2022	Complete solution for HR users to manage the onboarding process of eligible candidates	We need to create and customize the offer letter, approval process, ability for candidate to sign and accept as well as negotiate the offer	Take advantage of Rails event machine and Mustache placeholders for offer process, JS signature pad and canvas manipulation to capture candidate signature. Rendering and saving base64 image of the signatures to db.	2022-04-11 12:35:59.454419	2022-05-18 12:14:17.685944		https://scouttalentonboard.com/
+8	\N	Personal Website	2021	2022	A portfolio playground for showing my information and skillset	Creative website, integrate most interesting and cutting-edge technologies I learned recently	Incorporating dynamic data from local backend server and build up static website with NextJS, 3D layout with ThreeJS	2022-05-18 13:15:44.357135	2022-05-18 13:15:44.484558	https://github.com/hoang-phan/hoangphan-portfolio	
 \.
 
 
@@ -397,6 +496,55 @@ COPY public.schema_migrations (version) FROM stdin;
 20220405133049
 20220406125631
 20220405133259
+20220516033037
+20220516033038
+20220516033039
+20220516033040
+20220516033041
+20220516033042
+20220516033043
+\.
+
+
+--
+-- Data for Name: taggings; Type: TABLE DATA; Schema: public; Owner: hoang
+--
+
+COPY public.taggings (id, tag_id, taggable_type, taggable_id, tagger_type, tagger_id, context, created_at, tenant) FROM stdin;
+2	2	Project	6	\N	\N	tags	2022-05-18 12:36:46.062973	\N
+3	3	Project	6	\N	\N	tags	2022-05-18 12:36:46.078446	\N
+4	4	Project	6	\N	\N	tags	2022-05-18 12:36:46.094295	\N
+5	5	Project	6	\N	\N	tags	2022-05-18 12:36:46.109738	\N
+6	6	Project	7	\N	\N	tags	2022-05-18 13:06:08.751395	\N
+7	7	Project	7	\N	\N	tags	2022-05-18 13:06:08.76804	\N
+8	8	Project	7	\N	\N	tags	2022-05-18 13:06:08.784222	\N
+9	9	Project	7	\N	\N	tags	2022-05-18 13:06:09.776725	\N
+10	10	Project	7	\N	\N	tags	2022-05-18 13:06:09.794981	\N
+11	11	Project	8	\N	\N	tags	2022-05-18 13:15:44.407874	\N
+12	12	Project	8	\N	\N	tags	2022-05-18 13:15:44.42354	\N
+13	13	Project	8	\N	\N	tags	2022-05-18 13:15:44.440745	\N
+14	5	Project	8	\N	\N	tags	2022-05-18 13:15:44.455988	\N
+\.
+
+
+--
+-- Data for Name: tags; Type: TABLE DATA; Schema: public; Owner: hoang
+--
+
+COPY public.tags (id, name, created_at, updated_at, taggings_count) FROM stdin;
+1	https://github.com/hoang-phan/flappy_bird_ai	2022-05-18 12:34:55.679619	2022-05-18 12:34:55.679619	0
+2	JS	2022-05-18 12:36:46.01152	2022-05-18 12:36:46.01152	1
+3	Neural network	2022-05-18 12:36:46.01959	2022-05-18 12:36:46.01959	1
+4	AI	2022-05-18 12:36:46.026546	2022-05-18 12:36:46.026546	1
+6	Rails	2022-05-18 13:06:08.703443	2022-05-18 13:06:08.703443	1
+7	Python	2022-05-18 13:06:08.710577	2022-05-18 13:06:08.710577	1
+8	OpenCV	2022-05-18 13:06:08.718363	2022-05-18 13:06:08.718363	1
+9	Tesseract	2022-05-18 13:06:08.725038	2022-05-18 13:06:08.725038	1
+10	Algorithm	2022-05-18 13:06:08.733102	2022-05-18 13:06:08.733102	1
+11	NextJS	2022-05-18 13:15:44.373318	2022-05-18 13:15:44.373318	1
+12	ThreeJS	2022-05-18 13:15:44.379938	2022-05-18 13:15:44.379938	1
+13	3D Animation	2022-05-18 13:15:44.386494	2022-05-18 13:15:44.386494	1
+5	2D Animation	2022-05-18 12:36:46.03316	2022-05-18 12:36:46.03316	2
 \.
 
 
@@ -404,14 +552,14 @@ COPY public.schema_migrations (version) FROM stdin;
 -- Name: active_storage_attachments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hoang
 --
 
-SELECT pg_catalog.setval('public.active_storage_attachments_id_seq', 3, true);
+SELECT pg_catalog.setval('public.active_storage_attachments_id_seq', 6, true);
 
 
 --
 -- Name: active_storage_blobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hoang
 --
 
-SELECT pg_catalog.setval('public.active_storage_blobs_id_seq', 3, true);
+SELECT pg_catalog.setval('public.active_storage_blobs_id_seq', 6, true);
 
 
 --
@@ -439,7 +587,21 @@ SELECT pg_catalog.setval('public.companies_id_seq', 2, true);
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hoang
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 8, true);
+
+
+--
+-- Name: taggings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hoang
+--
+
+SELECT pg_catalog.setval('public.taggings_id_seq', 14, true);
+
+
+--
+-- Name: tags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hoang
+--
+
+SELECT pg_catalog.setval('public.tags_id_seq', 13, true);
 
 
 --
@@ -504,6 +666,22 @@ ALTER TABLE ONLY public.projects
 
 ALTER TABLE ONLY public.schema_migrations
     ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: taggings taggings_pkey; Type: CONSTRAINT; Schema: public; Owner: hoang
+--
+
+ALTER TABLE ONLY public.taggings
+    ADD CONSTRAINT taggings_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tags tags_pkey; Type: CONSTRAINT; Schema: public; Owner: hoang
+--
+
+ALTER TABLE ONLY public.tags
+    ADD CONSTRAINT tags_pkey PRIMARY KEY (id);
 
 
 --
@@ -584,6 +762,97 @@ CREATE INDEX index_projects_on_to_year ON public.projects USING btree (to_year);
 
 
 --
+-- Name: index_taggings_on_context; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_context ON public.taggings USING btree (context);
+
+
+--
+-- Name: index_taggings_on_tag_id; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_tag_id ON public.taggings USING btree (tag_id);
+
+
+--
+-- Name: index_taggings_on_taggable_id; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_taggable_id ON public.taggings USING btree (taggable_id);
+
+
+--
+-- Name: index_taggings_on_taggable_type; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_taggable_type ON public.taggings USING btree (taggable_type);
+
+
+--
+-- Name: index_taggings_on_taggable_type_and_taggable_id; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_taggable_type_and_taggable_id ON public.taggings USING btree (taggable_type, taggable_id);
+
+
+--
+-- Name: index_taggings_on_tagger_id; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_tagger_id ON public.taggings USING btree (tagger_id);
+
+
+--
+-- Name: index_taggings_on_tagger_id_and_tagger_type; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_tagger_id_and_tagger_type ON public.taggings USING btree (tagger_id, tagger_type);
+
+
+--
+-- Name: index_taggings_on_tagger_type_and_tagger_id; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_tagger_type_and_tagger_id ON public.taggings USING btree (tagger_type, tagger_id);
+
+
+--
+-- Name: index_taggings_on_tenant; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX index_taggings_on_tenant ON public.taggings USING btree (tenant);
+
+
+--
+-- Name: index_tags_on_name; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE UNIQUE INDEX index_tags_on_name ON public.tags USING btree (name);
+
+
+--
+-- Name: taggings_idx; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE UNIQUE INDEX taggings_idx ON public.taggings USING btree (tag_id, taggable_id, taggable_type, context, tagger_id, tagger_type);
+
+
+--
+-- Name: taggings_idy; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX taggings_idy ON public.taggings USING btree (taggable_id, taggable_type, tagger_id, context);
+
+
+--
+-- Name: taggings_taggable_context_idx; Type: INDEX; Schema: public; Owner: hoang
+--
+
+CREATE INDEX taggings_taggable_context_idx ON public.taggings USING btree (taggable_id, taggable_type, context);
+
+
+--
 -- Name: projects fk_rails_44a549d7b3; Type: FK CONSTRAINT; Schema: public; Owner: hoang
 --
 
@@ -597,6 +866,14 @@ ALTER TABLE ONLY public.projects
 
 ALTER TABLE ONLY public.active_storage_variant_records
     ADD CONSTRAINT fk_rails_993965df05 FOREIGN KEY (blob_id) REFERENCES public.active_storage_blobs(id);
+
+
+--
+-- Name: taggings fk_rails_9fcd2e236b; Type: FK CONSTRAINT; Schema: public; Owner: hoang
+--
+
+ALTER TABLE ONLY public.taggings
+    ADD CONSTRAINT fk_rails_9fcd2e236b FOREIGN KEY (tag_id) REFERENCES public.tags(id);
 
 
 --
